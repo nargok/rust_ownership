1a7bf6fd6ce9270dc50618c170c9e940ef0e6077
nargok <nargok@gmail.com>
2019-07-29 08:42:46 +0900

    関数と所有権

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 fn main() {
-    let s1 = String::from("hello");
-    let s2 = s1.clone();
+    let s1 = gives_ownership();
 
-    println!("s1 = {}, s2 = {}", s1, s2);
+    let s2 = String::from("hello");
+
+    let s3 = takes_and_gives_back(s2);
+}
+
+fn gives_ownership() -> String {
+    let some_string = String::from("hello");
+    some_string
+}
+
+fn takes_and_gives_back(a_string: String) -> String {
+    a_string
 }
